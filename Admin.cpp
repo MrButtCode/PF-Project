d4bc4c78f46e65139143fb0b316bcaf43996852e
MrButtCode <mrbutt.dev@gmail.com>
2025-01-24 23:12:37 +0500

    Seperation of folder

---
+++ Admin.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout << "Implement admin functionalities";
+	
+	return 0;
+
+}
---
+++ User.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout << "Implement User Functionalities";
+	
+	return 0;
+
+}
